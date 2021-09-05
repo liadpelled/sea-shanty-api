9ec86f8bcc172d77c9360ad1ad52b2571d2a88a1
Liad Pelled <liad435@gmail.com>
2021-09-05 14:32:07 +0300

    Added documentation

--- shantyscraper.py
+++ shantyscraper.py
@@ -7,12 +7,15 @@ import re
 from bs4 import BeautifulSoup
 
 
+# The website URL to be scraped
 url = "https://mainsailcafe.com/songs"
 
+# Scraped titles and lyrics will be stored in this dictionary
 shanties = {}
 
 #TODO DEAL WITH SPECIAL CHARACTERS IN URL
 def get_tree(url):
+    """This function uses the requests module to get a web page and returns its content to be later searched using XPath."""
     resp = requests.get(url=url, headers={
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
     })
@@ -24,6 +27,7 @@ def get_tree(url):
 
 
 def get_tree_bs4(url):
+    """This function uses the requests module to get a web page and returns its content to be later searched using BeautifulSoup."""
     resp = requests.get(url=url, headers={
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
     })
@@ -33,6 +37,7 @@ def get_tree_bs4(url):
 
 
 def scrape_song(url: str, song_name: str):
+    """This function uses XPath to scrape the lyrics of a shanty, given its URL."""
 
     print("Scraping song "+song_name+"...")
 
@@ -67,6 +72,8 @@ def scrape_song(url: str, song_name: str):
 
 
 def add_to_lyrics(song_lyrics, row):
+    """This function assits in dividing lyrics into 4 line verses, when the original lyrics appear in one large paragraph."""
+
     num_verses = len(song_lyrics)
 
     for i in range(num_verses):
@@ -79,6 +86,7 @@ def add_to_lyrics(song_lyrics, row):
 
 
 def scrape_song_bs4(url: str, song_name: str):
+    """This function uses BeautifulSoup to scrape the lyrics of a shanty, given its URL."""
 
     print("Scraping song " + song_name + "...")
 
@@ -91,38 +99,37 @@ def scrape_song_bs4(url: str, song_name: str):
         return
 
     soup = BeautifulSoup(song_tree, 'html.parser')
-    lyrics_divs = soup.find_all('div', class_ = 'lyrics')
+    lyrics_div = soup.find('div', class_ = 'lyrics')
 
     try:
-        for lyrics_div in lyrics_divs:
-            lyrics_ps = lyrics_div.find_all('p')
-            singlep = True if len(lyrics_ps)==1 else False
+        lyrics_ps = lyrics_div.find_all('p')
+        singlep = True if len(lyrics_ps)==1 else False
 
-            for item in lyrics_ps:
-                em_tags = item.find_all('em')
-                for emt in em_tags:
-                    emt.replace_with(emt.text)
-                
-                strong_tags = item.find_all('strong')
-                for strt in strong_tags:
-                    strt.replace_with(strt.text)
-                
-                str_item = str(item)[3:-4].replace('<br/>', '&')
-                if str_item[-1] == '&':
-                    str_item = str_item[:-1]
-                str_item = re.sub(r'&+', r'&', str_item)
-                str_item = re.sub(r'Ch:', '', str_item)
-                lyrics_rows = str_item.split('&')
+        for item in lyrics_ps:
+            em_tags = item.find_all('em')
+            for emt in em_tags:
+                emt.replace_with(emt.text)
+            
+            strong_tags = item.find_all('strong')
+            for strt in strong_tags:
+                strt.replace_with(strt.text)
+            
+            str_item = str(item)[3:-4].replace('<br/>', '&')
+            if str_item[-1] == '&':
+                str_item = str_item[:-1]
+            str_item = re.sub(r'&+', r'&', str_item)
+            str_item = re.sub(r'Ch:', '', str_item)
+            lyrics_rows = str_item.split('&')
 
-                if singlep:
-                    for row in lyrics_rows:
-                        add_to_lyrics(song_lyrics, row.strip())
-                else:
-                    for row in lyrics_rows:
-                        if verse == len(song_lyrics):
-                            song_lyrics.append([])
-                        song_lyrics[verse].append(row.strip())
-                    verse += 1
+            if singlep:
+                for row in lyrics_rows:
+                    add_to_lyrics(song_lyrics, row.strip())
+            else:
+                for row in lyrics_rows:
+                    if verse == len(song_lyrics):
+                        song_lyrics.append([])
+                    song_lyrics[verse].append(row.strip())
+                verse += 1
     except IndexError as e:
         print(f"Error scraping song: {e}")
         return
@@ -131,6 +138,7 @@ def scrape_song_bs4(url: str, song_name: str):
         shanties[song_name] = song_lyrics
 
 
+# Go through 9 pages on the website
 for i in range(9):
     if i==0:
         tree = get_tree(url)
@@ -151,6 +159,7 @@ for i in range(9):
 
         scrape_song_bs4(song_url, song_name)
 
-        
-with open('shanties_bs4_2.json', 'w') as fp:
+
+# Save the scraped shanties to a JSON file         
+with open('shanties.json', 'w') as fp:
     json.dump(shanties, fp)
